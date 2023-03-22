ba9a793d2f70f62cb9cefe076ed33f8a6c480f9e
hieuvominh <mvohieu@gmail.com>
2023-03-22 20:52:21 +0700

    first commit

--- wp-config.php
+++ wp-config.php
@@ -36,7 +36,8 @@ define('DB_CHARSET', 'utf8mb4');
 
 /** The database collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
+define('WP_HOME', 'https://xedep.herokuapp.com/');
+define('WP_SITEURL', 'https://xedep.herokuapp.com/');
 /**#@+
  * Authentication unique keys and salts.
  *
